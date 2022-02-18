ed25976439557e7a3a0e0b5fdc566af95278a167
enieber cunha <eniebercunha@gmail.com>
2022-02-17 23:38:42 -0300

    feat: create structure with react-navigation + ant design

--- App.js
+++ App.js
@@ -1,20 +1,34 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import React, { useState, useEffect } from "react";
+import { AppLoading, Font } from "expo";
+import { Provider } from "@ant-design/react-native";
+import { AuthProvider } from "./src/services/auth";
+import RootNavigator from "./src/navigation";
 
-export default function App() {
+export default function App(props) {
+  const [isRedy, setRedy] = useState(false);
+
+  useEffect(() => {
+    (async () => {
+      setRedy(true);
+    })();
+  }, []);
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <AuthProvider>
+      <Provider>
+        <RootNavigator />
+      </Provider>
+    </AuthProvider>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
